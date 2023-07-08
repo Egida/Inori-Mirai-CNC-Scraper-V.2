4a03aa0e30a40345c8283ebc45e1ba7472c430ce
Lore <88983497+PyLore@users.noreply.github.com>
2023-07-08 00:30:16 -0400

    Update portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -7,7 +7,8 @@ from socket import (
     AF_INET,
     SOCK_STREAM
 )
-import time
+
+from time import time
 
 class Colors:
     WHITE:     str = '\033[38;2;255;255;255m'
@@ -18,10 +19,10 @@ class Colors:
     LIGHTPINK: str = '\033[38;2;255;150;255m'
 
 class Scanner:
-    prange: dict = {
+    prange: list[int] = [
         number for number in range(10000) if number not in 
         (21, 22, 23, 25, 53, 69, 80, 443, 3074, 3306, 5060, 9307)
-    }
+    ]
     
 
     def __init__(self: Scanner, addr: str = '127.0.0.1') -> None:
@@ -49,32 +50,41 @@ class Scanner:
         self.results.clear()
 
         with ThreadPoolExecutor(max_workers=800) as executor:
+            tuple(executor.submit(self.__connect) for _ in range(800))
+
+
+    def __connect(self: Scanner) -> None:
+        while self.prange:
+            
             for port in self.prange:
-                executor.submit(self.__connect, port)
+                if port in self.prange:
+                    try:
+                        self.prange.remove(port)
+                    except:
+                        continue
+                
+                with socket(AF_INET, SOCK_STREAM) as sock:
+                    sock.settimeout(2.0)
+                    try:
+                        sock.connect((self.addr, port))
+                    except:
+                        continue
 
-
-    def __connect(self: Scanner, port: int) -> None:
-    
-        with socket(AF_INET, SOCK_STREAM) as sock:
-        
-            sock.settimeout(2.0)
-
-            try:
-                sock.connect((self.addr, port))
                 self.results.append(port)
-            except ConnectionRefusedError or TimeoutError:
-                pass
                 
 if __name__ == '__main__':
     handler: Scanner = Scanner()
-    while 1:
+    while True:
         print(f'{Colors.LIGHTPINK}Scanning for services...{Colors.WHITE}')
         handler.addr: str = input(f'{Colors.PINK}ADDR:{Colors.WHITE} ')
-        start:        int = time.time()
+        start:        int = time()
+
         handler.get_results()
+
         if not handler.results:
             print(f'{Colors.RED}Failed to find services.{Colors.WHITE}\n')
         else:
             print(f'{Colors.WHITE}{handler.addr}: {f"{Colors.WHITE}, ".join(f"{Colors.LIGHTRED}{str(port)}" for port in handler.results)}\n')
-        end: int = time.time()
+
+        end: int = time()
         print(f'{Colors.RED}Execution time: {Colors.PINK}{end - start} {Colors.RED}seconds.')
