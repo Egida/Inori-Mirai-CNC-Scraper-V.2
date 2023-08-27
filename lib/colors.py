a99f8d4dbc701cd5db7f67dae4196f3a08d47185
Lore <88983497+PyLore@users.noreply.github.com>
2023-08-27 02:50:57 -0400

    Update colors.py

--- lib/colors.py
+++ lib/colors.py
@@ -1,7 +1,7 @@
 class Colors:
-    WHITE:     str = '\033[38;2;255;255;255m'
+    LIGHTPINK: str = '\033[38;2;255;100;255m'
+    LIGHTRED:  str = '\033[38;2;255;25;50m'
     PINKRED:   str = '\033[38;2;255;50;75m'
+    WHITE:     str = '\033[38;2;255;255;255m'
     LIME:      str = '\033[38;2;0;255;152m'
     RED:       str = '\033[38;2;255;0;0m'
-    LIGHTRED:  str = '\033[38;2;255;25;50m'
-    LIGHTPINK: str = '\033[38;2;255;100;255m'
